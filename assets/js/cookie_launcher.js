957aaf1e09ee157506589dbfae53b3674078c530
dev-Laky <75489200+dev-Laky@users.noreply.github.com>
2023-03-12 16:16:57 +0100

    update: cookie test

---
+++ assets/js/cookie_launcher.js
@@ -0,0 +1,48 @@
+/*  ---------------------------------------------------
+    Template Name: Manup
+    Description: Manup Event HTML Template
+    Author: Colorlib
+    Author URI: http://colorlib.com
+    Version: 1.0
+    Created: Colorlib
+---------------------------------------------------------  */
+
+'use strict';
+
+(function ($) {
+
+    function setCookie(cname, cvalue, exdays) {
+        const d = new Date();
+        d.setTime(d.getTime() + (exdays*24*60*60*1000));
+        let expires = "expires="+ d.toUTCString();
+        document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+        /*console.log(document.cookie);*/
+    }
+
+    function getCookie(cname) {
+        let name = cname + "=";
+        let decodedCookie = decodeURIComponent(document.cookie);
+        let ca = decodedCookie.split(';');
+        for(let i = 0; i <ca.length; i++) {
+          let c = ca[i];
+          while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+          }
+          if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+          }
+        }
+        return "";
+    }
+
+    document.getElementById("submit-cookies").onclick = setCookie("cookies", "true", 365);
+
+    console.log(document.cookie);
+    let cookie = getCookie("cookies");
+    if(cookie != "true"){
+        $(document).ready(function(){
+            $("#cookieconsent3").modal('show');
+        });
+    }
+
+})(jQuery);
