266041efd492c29f5b19ff1c0e9b95be171e0877
angighn <angelina.gruhn70@gmail.com>
2023-03-14 22:52:04 +0100

    update: cookie final

--- assets/js/cookie_manage.js
+++ assets/js/cookie_manage.js
@@ -2,6 +2,14 @@
 
 (function ($) {
 
+    function setCookie(cname, cvalue, exdays) {
+        const d = new Date();
+        d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+        let expires = "expires=" + d.toUTCString();
+        document.cookie = cname + "=" + cvalue + ";" + expires + "; path=/;";
+        /*console.log(document.cookie);*/
+    }
+
     document.getElementById("submit-cookie-delete").addEventListener('click',function (){
         /* Cookie remove */
         document.cookie = "cookies=true; max-age=0;";
@@ -9,7 +17,7 @@
         window.location.reload();
     });
 
-    document.getElementById("submit-cookies-datenschutz").addEventListener('click',function (){
+    document.getElementById("submit-cookies-datenschutz").addEventListener('click',function(){
         setCookie("cookies", "true", 365);
     }); 
 
