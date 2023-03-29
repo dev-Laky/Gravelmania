c283904640f3265f5442ef93b68bf6a4f8c8302b
angighn <angelina.gruhn70@gmail.com>
2023-03-29 21:13:55 +0200

    update: webkey updated

--- assets/js/form/recaptcha.js
+++ assets/js/form/recaptcha.js
@@ -1,6 +1,8 @@
 var onloadCallback = function() {
   grecaptcha.render('recaptcha', {
-    'sitekey' : '6LfD_CslAAAAAJP4kdjZ3oTIq53U0B5e00siEWhW'
+    'sitekey' : '6LcKjUElAAAAAF1Llm3C0bB-UDwzMj9Tui2AVPuk',
+    'callback' : verifyCallback,
+    'theme' : 'dark'
   });
 };
 
